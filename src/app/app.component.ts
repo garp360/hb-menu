bed7c03beb7f033de8dc9dc9eb6edc3c407ada6f
Garth Pidcock <garthpidcock@garths-imac.attlocal.net>
2017-11-21 00:49:13 -0500

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from './menu-item';
 import { Observable } from 'rxjs/Observable';
 import { MenuService } from './menu/menu.service';
 
 @Component({
   selector: 'conops-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent implements OnInit {
@@ -15,9 +13,7 @@ export class AppComponent implements OnInit {
   title = 'app';
   menuItems: MenuItem[];
 
-  constructor( private menuService:MenuService ) {
-
-  }
+  constructor( private menuService:MenuService ) { }
 
   ngOnInit() {
     this.menuService.load().subscribe(res => {
@@ -25,75 +21,4 @@ export class AppComponent implements OnInit {
       this.menuItems = res;
     });
   }
-
-  // menuItems: MenuItem[] =
-  // [
-  //   {
-  //     text: 'home',
-  //     children:
-  //     [
-  //       {
-  //         text: 'Engineering',
-  //         children:
-  //         [
-  //           {
-  //             text: 'ENG-16'
-  //           },
-  //           {
-  //             text: 'Route Builder',
-  //             url: 'https://www.google.com'
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         text: 'Inventory',
-  //         children:
-  //         [
-  //           {
-  //             text: 'INV-16'
-  //           },
-  //           {
-  //             text: 'PSA'
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         text: 'Administration',
-  //         children:
-  //         [
-  //           {
-  //             text: 'CONOPS',
-  //             children:
-  //             [
-  //               {
-  //                 text: 'Groups'
-  //               },
-  //               {
-  //                 text: 'Users',
-  //                 url: 'http://www.msn.com',
-  //                 children:
-  //                 [
-  //                   {
-  //                     text: 'Unlock',
-  //                     url: 'http://www.yahoo.com',
-  //                   },
-  //                   {
-  //                     text: 'Add',
-  //                     url: 'http://www.aol.com',
-  //                   }
-  //                 ]
-  //               },
-  //               {
-  //                 text: 'Permissions'
-  //               }
-  //             ]
-  //           },
-  //           {
-  //             text: 'MRO'
-  //           }
-  //         ]
-  //       }
-  //     ]
-  //   }
-  // ];
 }
